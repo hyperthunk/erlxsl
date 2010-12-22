984222c16c577cf5a0a8bb2c18e71f9720bfe8a1
Tim Watson <watson.timothy@gmail.com>
2010-12-22 16:57:31 +0000

    getting semblance of order - compile now works. :)

--- c_src/erlxsl_api.h
+++ c_src/erlxsl_api.h
@@ -47,223 +47,212 @@ extern "C" {
 #define DebugOut(message, args...) /* DEBUG OUTPUT DISABLED */
 #endif
 
-#define SET_STATUS(res, stat) \
-	((transform_job*)((request_context*)(res->context))->job)->status = stat;
-
 	// TODO: investigate using the Apache Portable Runtime
 	
-    /* Data Types & Aliasing */
-    
-    typedef unsigned char           UInt8;
-    typedef unsigned short  int     UInt16;
-    typedef unsigned short  int     UInt32;
-    typedef signed   char           Int8;
-    typedef signed   short  int     Int16;
-    //this (following) word size assumption is true in the erlang source, so we should be ok!
-    typedef signed   long   int     Int32;
-    typedef signed   long   long    Int64;
-    //this is platform dependant but consistent with erts
-    typedef unsigned long           ULong;
-    
-    /*
-     * Packages information about a parameter (its name and value)
-     * and the next parameter in line - i.e. this is a linked list.
+  /* Data Types & Aliasing */
+  
+  typedef unsigned char           UInt8;
+  typedef unsigned short  int     UInt16;
+  typedef unsigned short  int     UInt32;
+  typedef signed   char           Int8;
+  typedef signed   short  int     Int16;
+  // this (following) word size assumption is true in the erlang 
+// source, so we should be ok!
+  typedef signed   long   int     Int32;
+  typedef signed   long   long    Int64;
+  //this is platform dependant but consistent with erts
+  typedef unsigned long           ULong;
+  
+  /*
+   * Packages information about a parameter (its name and value)
+   * and the next parameter in line - i.e. this is a linked list.
+   */
+  typedef struct {
+	  /*
+	   * The name of the parameter (as passed to xslt).
+	   */
+	  char * name;
+	  /*
+	   * The value assigned to the parameter (as passed to xslt).
+	   */
+	  char * value;
+	  /*
+	   * Stores a pointer to the next parameter, or NULL if this
+	   * element is the tail.
+	   */
+	  void* next;
+  } param_info;
+  
+  //TODO: supply explicit values here and (perhaps) use a bitmask to allow
+  //      setting values such as ProviderError | FatalError
+  //      [or perhaps have a separate ErrorState enum!?]
+  typedef enum {
+     /* Set when everything *looks* fine - this is no guarantee of
+     * consistent data however!
      */
-    typedef struct {
-        /*
-         * The name of the parameter (as passed to xslt).
-         */
-        char * name;
-        /*
-         * The value assigned to the parameter (as passed to xslt).
-         */
-        char * value;
-        /*
-         * Stores a pointer to the next parameter, or NULL if this
-         * element is the tail.
-         */
-        void* next;
-    } param_info;
-    
-    //TODO: supply explicit values here and (perhaps) use a bitmask to allow
-    //      setting values such as ProviderError | FatalError
-    //      [or perhaps have a separate ErrorState enum!?]
-    typedef enum {
-        /* Set when everything *looks* fine - this is no guarantee of
-         * consistent data however!
-         */
-        Ok,
-        /*
-         * Set when the input headers (a prelude containing parameter size
-         * and input type flags, set by the caller) is in an inconsistent format
-         * and/or contains inconsistent data (e.g. negative flags/size).
-         */
-        InconsistentInputHeaders,
-        /*
-         * Set when the expected buffer outline size exceeds the supplied
-         * buffer length. NB: Not sure if this will ever happen!
-         */
-        BufferSizeMismatch,
-        /*
-         * Set by providers, to let the driver know they've run into a problem.
-         */
-        ProviderError,
-        /*
-         * Set generally whenever a fatal runtime error occurs. 
-         */
-        FatalError
-    } DriverState;
+    Ok,
+    /*
+     * Set when the input headers (a prelude containing parameter size
+     * and input type flags, set by the caller) is in an inconsistent format
+     * and/or contains inconsistent data (e.g. negative flags/size).
+     */
+    InconsistentInputHeaders,
+    /*
+     * Set when the expected buffer outline size exceeds the supplied
+     * buffer length. NB: Not sure if this will ever happen!
+     */
+    BufferSizeMismatch,
+    /*
+     * Set by providers, to let the driver know they've run into a problem.
+     */
+    ProviderError,
+    /*
+     * Set generally whenever a fatal runtime error occurs. 
+     */
+    FatalError,
+		/*
+		 * Set when the driver has exhausted available heap space.
+		 */
+		OutOfMemory
+  } DriverState;
 
-	typedef enum {
+	typedef enum input_type {
 		File = 1,
 		Buffer = 2,
 		Stream = 3
 	} InputType;
+	
+	typedef enum { Binary, Text } ResultFormat;
     
+  /*
+   * transform_request packages the data used during
+   * transformation, which is (usually) gathered from the
+   * input buffer supplied by the Erlang runtime during
+   * a call to one of the control/input callback functions,
+   * with some useful flags.
+   */
+  typedef struct {
+      /*
+       * Denotes the kind of input (e.g. buffer, stream or file based) for the data.
+       */
+      InputType inputKind;
+      /*
+       * Denotes the kind of xsl input (e.g. buffer, stream 
+ 		 * or file based) for the stylesheet. */
+      InputType xslInputKind;
+      /*
+       * A pointer to the head element in a linked list of param_info structures.
+       */
+      param_info* parameters;
+      /*
+       * A pointer to the input data.
+       */
+      char * inputData;
+      /*
+       * A pointer to the stylesheet data.
+       */
+      char * stylesheetData;
+  } transform_job;
+  
+  /* comms related data structures (e.g. for interacting with the emulator) */    
+  
+  /*
+   * Stores the current request context, which consists of the port,
+   * target process id (note that this doesn't have to be the Pid of 
+   * the *connected process* if you want to send data to another Pid) and
+   * a pointer to the current request.
+   */
+  typedef struct {
+      /* Stores the port associated with the current driver instance. */
+      void* port;
+      /* Stores the calling process' Pid. */
+      unsigned long callerPid;
+      /* Stores a pointer to the current request data. */
+      transform_job* job;
+  } request_context;
+    
+  /*
+   * Stores response data operated on when calling back into the emulator.
+   */
+  typedef struct {
+    /* Stores the current request context. */
+    request_context* context;
     /*
-     * transform_request packages the data used during
-     * transformation, which is (usually) gathered from the
-     * input buffer supplied by the Erlang runtime during
-     * a call to one of the control/input callback functions,
-     * with some useful flags.
-     */
-    typedef struct {
-        /*
-         * Holds the status of the current request. This is also a hint as
-         * to whether or not there is any useful information in the errorMessage field.
-         */
-        DriverState status;
-        /*
-         * If anything went wrong with request processing, this field will
-         * (hopefully) contain a helpful error message.
-         */
-        char * errorMessage;
-        /*
-         * Denotes the kind of input (e.g. buffer, stream or file based) for the data.
-         */
-        InputType inputKind;
-        /*
-         * Denotes the kind of xsl input (e.g. buffer, stream or file based) for the stylesheet.
-         */
-        InputType xslInputKind;
-        /*
-         * A pointer to the head element in a linked list of param_info structures.
-         */
-        param_info* parameters;
-        /*
-         * A pointer to the input data.
-         */
-        char * inputData;
-        /*
-         * A pointer to the stylesheet data.
-         */
-        char * stylesheetData;
-    } transform_job;
-    
-    /* comms related data structures (e.g. for interacting with the emulator) */    
-    
+     * Holds the status of the current request. This is also a hint as
+     * to whether or not there is any useful information 
+   	 * in the errorMessage field. */
+    DriverState status;
     /*
-     * Describes the mode in which an xslt library adapter can 
-     * pass response data to be returned to the emulator.
+     * If anything went wrong with request processing, this field will
+     * (hopefully) contain a helpful error message.
      */
-    typedef enum {
-        /* Indicates that data is passed in a buffer (i.e. char*). */
-        Buffer = 1,
-        /* Indicates that data is passed using the driver term format. */
-        DriverTerm = 2,
-		/* Indicates that data is passed using the external term format. */
-		ExternalTermFormat = 3
-    } ResponseFormat;
-    
-    /*
-     * Stores the current request context, which consists of the port,
-     * target process id (note that this doesn't have to be the Pid of 
-     * the *connected process* if you want to send data to another Pid) and
-     * a pointer to the current request.
+    char * errorMessage;
+    /* Stores the response size (either the buffer length or the 
+     * number of terms in the supplied composite ErlDrvTermData array).
+     * NB: This field is optional, as the protocol implementation performs
+     * the same bounds checking regardless.
      */
-    typedef struct {
-        /* Stores the port associated with the current driver instance. */
-        void* port;
-        /* Stores the calling process' Pid. */
-        unsigned long callerPid;
-        /* Stores a pointer to the current request data. */
-        transform_job* job;
-    } request_context;
-    
-    /*
-     * Stores response data operated on when calling back into the emulator.
-     */
-    typedef struct {
-        /* Stores the current request context. */
-        request_context* context;
-        /* Specifies the response format. 
-         Used to decode which field in #response we're 
-         meant to be looking at. */
-        ResponseFormat responseFormat;
-        /* Stores the response size (either the buffer length or the 
-         * number of terms in the supplied composite ErlDrvTermData array).
-         * NB: This field is optional, as the protocol implementation performs
-         * the same bounds checking regardless.
-         */
-        int responseSize;
-        /* Stores the response data itself. - TODO: this feels rather "unportable" as an approach */
-        union { 
-            /* Maintaining our response data in a buffer. */
-            char* buffer;
-            /* Maintaining our response data in the *driver term format*; this
-             is a pointer to the start of an array in the appropriate format.*/
-            void* data;
-        } payload;
-    } transform_result;
+    int size;
+		/* The format in which the resulting data is stored */
+		ResultFormat format;
+    /* Stores the response data itself. */
+    void* payload;
+  } transform_result;
 
 /************************  THE EXTERNAL API ***********************************/
     
-    /*
-     * Gives the implementation provider a chance to initialize.
-     * Returns a value from the 'DriverState' enumeration to indicate
-     * current system state. A return value of DriverState::ProviderError
-     * will cause the driver to exit, which the erlang port handler will
-     * interpret as and error and unload the driver library.
-     */
-    typedef DriverState (*InitFunc)();
+  /*
+   * Gives the implementation provider a chance to initialize.
+   * Returns a value from the 'DriverState' enumeration to indicate
+   * current system state. A return value of DriverState::ProviderError
+   * will cause the driver to exit, which the erlang port handler will
+   * interpret as and error and unload the driver library.
+   */
+  typedef DriverState InitFunc(void* state);
 
-    /*
-     * This function is the request handler hook used by plugins (such as 
-     * implementations for sablotron and/or libxslt).
-     * 
-     * The response structure passed contains the context required to get to
-     * underlying data, along with fields which the plugin can fill in to 
-     * get data sent back to erlang.
-     *
-     */
-    typedef void (*TransformFunc)(void* result); 
+  /*
+   * This function is the request handler hook used by plugins (such as 
+   * implementations for sablotron and/or libxslt).
+   * 
+   * The response structure passed contains the context required to get to
+   * underlying data, along with fields which the plugin can fill in to 
+   * get data sent back to erlang.
+   *
+   */
+  typedef void TransformFunc(void* result); 
+  
+  /*
+   * This function gives the implementing plugin a chance to cleanup 
+   * after handle_request has returned. 
+   *
+   * Returns a value from the 'DriverState' enumeration to indicate
+   * current system state. A return value of DriverState::ProviderError
+   * will cause the driver to exit, which the erlang port handler will
+   * interpret as and error and unload the driver library.
+   */
+  typedef DriverState PostTransformFunc(void* result);
     
-    /*
-     * This function gives the implementing plugin a chance to cleanup 
-     * after handle_request has returned. 
-     *
-     * Returns a value from the 'DriverState' enumeration to indicate
-     * current system state. A return value of DriverState::ProviderError
-     * will cause the driver to exit, which the erlang port handler will
-     * interpret as and error and unload the driver library.
-     */
-    typedef DriverState (*PostTransformFunc)(void* result);
-    
-    /*
-     * Gives the implementation provider a change to cleanup. Because this function
-     * is only called when the driver is being stopped, the provider cannot supply
-     * a meaningful return value, therefore the function returns void.
-     */
-    typedef void (*ShutdownFunc)();
+  /*
+   * Gives the implementation provider a change to cleanup. Because this function
+   * is only called when the driver is being stopped, the provider cannot supply
+   * a meaningful return value, therefore the function returns void.
+	 *
+	 * After this function returns, the memory allocated for the xsl_engine will be
+	 * freed, which means that the engine must fully release all references/pointers 
+	 * held before returning.
+   */
+   typedef void ShutdownFunc(void* state);
    
+	/* Represents an XSLT engine. */
 	typedef struct {
-		/* The following function pointers will need be set by the provider on startup */
-		InitFunc 			initialize;
-		TransformFunc 		handleTransform;
-		PostTransformFunc	postHandle;
-		ShutdownFunc		shutdown;
+		/* The following function pointers will need 
+		   be set by the provider on startup */
+		InitFunc 						*initialize;
+		TransformFunc 			*transform;
+		PostTransformFunc		*afterTransform;
+		ShutdownFunc				*shutdown;
 		/* Generic storage location, so providers can stash whatever they need. */
-        void* providerData;
+    void* 							providerData;
 	} xsl_engine;
 
 /******************************************************************************/    
--- c_src/erlxsl_drv.c
+++ c_src/erlxsl_drv.c
@@ -33,97 +33,135 @@
  *
  */
 
-#include "erlxsl_drv.h"
+#include <erl_driver.h>
+#include "erlxsl_api.h"
 
-/* internal data structures */
+/* INTERNAL DATA & DATA STRUCTURES */
 
 typedef struct {
-    ErlDrvPort  port;
+  ErlDrvPort  port;
 	xsl_engine* provider;
 } driver_spec;
 
-/* erts driver callbacks */
+static ErlDrvTermData atom_result; 
+static ErlDrvTermData atom_error;
+
+/* FORWARD DEFS */
+
+static void init_provider(driver_spec*, char *);
+static DriverState default_initialize(void*);
+static void default_handleTransform(void*);
+static DriverState default_postHandle(void*);
+static void default_shutdown(void*);
+static ErlDrvData start_driver(ErlDrvPort, char*);
+static void stop_driver(ErlDrvData);
+static void outputv(ErlDrvData, ErlIOVec*);
+static void ready_async(ErlDrvData, ErlDrvThreadData);
+static void cleanup_job(transform_job* job);
+
+#define DRV_FREE(x) if (NULL != x) driver_free(x)
+
+#define READ_Int32(s)  ((((int)(((unsigned char*) (s))[0]))  << 24) | \
+                        (((int)(((unsigned char*) (s))[1]))  << 16) | \
+                        (((int)(((unsigned char*) (s))[2]))  << 8)  | \
+                        (((int)(((unsigned char*) (s))[3]))))
+
+/* INTERNAL DRIVER FUNCTIONS */
+
+static void init_provider(driver_spec* drv, char* buff) {
+	xsl_engine* engine = (xsl_engine*)driver_alloc(sizeof(xsl_engine));
+	if (engine == NULL) {
+		return;
+	}
+	engine->initialize = default_initialize;
+	engine->transform = default_handleTransform;
+	engine->afterTransform = default_postHandle;
+	engine->shutdown = default_shutdown;
+	
+	// NB: this is a temporary structure for testing/discovery
+	drv->provider = engine;
+};
+
+static DriverState default_initialize(void* state) {
+	return Ok;
+};
+
+static void default_handleTransform(void* result) {
+  transform_result* res = (transform_result*)result;
+	char* data;
+  data = "<response><name>This is very silly!</name><value>NUTS</value></response>";
+  res->payload = data;
+  res->status = Ok;
+};
+
+static DriverState default_postHandle(void* result) {
+  return Ok;
+};
+
+static void default_shutdown(void* state) {};
+
+static void cleanup_job(transform_job* job) {
+	param_info* next; 
+	param_info* current;
+	
+  if (NULL != job) {
+		DRV_FREE(job->inputData);
+		DRV_FREE(job->stylesheetData);
+    
+	  current = job->parameters;
+		while (NULL != current) {
+		  next = (param_info*)current->next;
+		  DRV_FREE(current->name);
+		  DRV_FREE(current->value);
+		  current = next;
+		}
+	
+		DRV_FREE(job);
+  }
+}
+
+/* DRIVER CALLBACK FUNCTIONS */
 
 static ErlDrvData start_driver(ErlDrvPort port, char *buff) {
     
-    // avoid total madness! nice tip that one...
-    if (port == NULL) {
-        return ERL_DRV_ERROR_GENERAL;
-    }
+	atom_result = driver_mk_atom("result");
+	atom_error  = driver_mk_atom("error");
 
-    // configure erts
-    driver_spec* d = (driver_spec*)driver_alloc(sizeof(driver_spec));
+  // avoid total madness! nice tip that one...
+  if (port == NULL) {
+      return ERL_DRV_ERROR_GENERAL;
+  }
+
+  driver_spec* d = (driver_spec*)driver_alloc(sizeof(driver_spec));
 	if (d == NULL) {
 		return ERL_DRV_ERROR_GENERAL; // TODO: use ERL_DRV_ERROR_ERRNO and provide out-of-memory info
 	}
-    d->port = port;    
+  d->port = port;    
 
 	init_provider(d, buff);
-	if (d->engine == NULL) {
+	if (d->provider == NULL) {
 		return ERL_DRV_ERROR_GENERAL; // TODO: use ERL_DRV_ERROR_ERRNO and provide out-of-memory info
 	}
-	DriverState state = (d->engine)->initialize();
-	if (state == DriverState.Ok) {
+	DriverState state = (d->provider)->initialize(d);
+	if (state == Ok) {
 		return (ErlDrvData)d;
 	} else {
 		return ERL_DRV_ERROR_GENERAL; // TODO: use ERL_DRV_ERROR_ERRNO and provide more info
 	}
 }
 
-static void stop_driver(ErlDrvData handle) {
+static void stop_driver(ErlDrvData drv_data) {
+  // give the provider a chance to clean up
+  driver_spec* d = (driver_spec*)drv_data;
+	ErlDrvPort port = d->port;
+	xsl_engine* engine = d->provider;
+	void* state = &port;
+	engine->shutdown(state);
 
-    // give the provider a chance to clean up
-    // destroy_provider();
-
-    // cleanup for erts
-    driver_free((char*)handle);
+  // driver cleanup
+	driver_free((char*)engine);
+  driver_free((char*)drv_data);
 }
-
-/* 
- 
-This output handler function is called from erlang with erlang:port_command(Port, Payload).
-static void output(ErlDrvData handle, char *buff, int bufflen) {
-    driver_spec* globalDriverSpecData;
-    ErlDrvPort port;
-    
-    globalDriverSpecData = (driver_spec*)handle;
-    port = globalDriverSpecData->port;
-    
-#ifdef DEBUG
-    fprintf(stderr, "Driver received buffer of size %i\n", ((char *)&bufflen));
-#endif
-    
-    ErlDrvTermData callerPid = driver_caller(port);
-    
-    RequestContext* context;
-    context = driver_alloc(sizeof(RequestContext));
-    if (NULL == context) {
-        E_OUT_OF_MEMORY;
-        return;
-    }
-    
-    context->port = port;
-    context->callerPid = callerPid;
-    TransformRequestPtr request = unpackRequest((const char *)buff, bufflen);
-    if (NULL == request) { //NB: this only happens when the heap is exhausted!
-        releaseContext(context);
-        E_OUT_OF_MEMORY;
-        return;
-    }
-    
-    context->request = request;    
-    TransformResponse* response = malloc(sizeof(TransformResponse));
-    if (NULL == response) {
-        releaseContext(context);
-        E_OUT_OF_MEMORY;
-        return;
-    }
-    
-    response->context = context;
-    long taskHandle = driver_async(port, NULL, handle_request, response, NULL);
-}
-
-*/
 
 static void outputv(ErlDrvData drv_data, ErlIOVec *ev) {
 	driver_spec* d = (driver_spec*)drv_data;
@@ -134,76 +172,107 @@ static void outputv(ErlDrvData drv_data, ErlIOVec *ev) {
 	int xmlLen, xslLen;
 	int paramCount;
 	
-	xmlKind = read_int32(&(ev->binv[1]->orig_bytes[1]));
-	xslKind = read_int32(&(ev->binv[1]->orig_bytes[2]));
-	xmlLen = read_int32(&(ev->binv[1]->orig_bytes[3]));
-	xslLen = read_int32(&(ev->binv[1]->orig_bytes[4]));
+	xmlKind = READ_Int32(&(ev->binv[1]->orig_bytes[1]));
+	xslKind = READ_Int32(&(ev->binv[1]->orig_bytes[2]));
+	xmlLen  = READ_Int32(&(ev->binv[1]->orig_bytes[3]));
+	xslLen  = READ_Int32(&(ev->binv[1]->orig_bytes[4]));
 	
-	long taskHandle = driver_async(port, NULL, engine->handleTransform, ev, NULL); //, engine->postHandle);
+	driver_async(port, NULL, engine->transform, ev, NULL); //, engine->postHandle);
 };
 
+/*
+ * Processes the supplied transform_result and sends it to the appropriate
+ * erlang process (e.g. the specified receiver)
+ */
 static void ready_async(ErlDrvData drv_data, ErlDrvThreadData async_data) {
-    driver_spec* driverBundle = (driver_spec*)drv_data;
-    ErlDrvPort port = driverBundle->port;
-    
-    TransformResponse* response = (TransformResponse*)async_data;
-    sendResponse(response, NULL);    
+	int 								response_len;
+	ErlDrvTermData* 		term;
+	ErlDrvTermData 			calleePid, tag;
+	ErlDrvPort 					port;
+	driver_spec* 				driverBundle;
+	xsl_engine* 				provider;
+	transform_result* 	result;
+	request_context* 		context;
+	void* 							payload;		// used to process the payload
+	transform_job* 			job;				// used to repeatedly refer to the completed job 
+	DriverState					state;
+	ErlDrvTermData 			spec[4];
+	
+	// ErlDrvPort port, ErlDrvTermData receiver, ErlDrvTermData* term, int n
+	
+	driverBundle 	= (driver_spec*)drv_data;
+	provider	 		= (xsl_engine*)driverBundle->provider;
+  port		 	 		= driverBundle->port;
+  result 		 		= (transform_result*)async_data;
+	context 	 		= result->context;
+	job		 	 			= context->job;
+	state		 			= result->status;
+	port 		 			= (ErlDrvPort)context->port;
+	calleePid 	 	= (ErlDrvTermData)context->callerPid;
+	
+	switch (state) {
+	case Ok:
+		payload = result->payload;
+		tag	= atom_result;
+		break;
+	default:  // TODO: it would be better if we didn't do "everthing else is an error" here
+		payload = result->errorMessage;
+		tag = atom_error;
+	}
+	
+	spec[0] = ERL_DRV_ATOM, tag;
+	spec[1] = ERL_DRV_PORT, driver_mk_port(port);
+  //isn't this next component an implicit ErlDrvBinary and therefore requires a call to driver_free_binary?
+	if (result->format == Binary) {
+		spec[2] = ERL_DRV_BINARY, (ErlDrvBinary*)payload, (ERL_DRV_UINT, result->size);
+	} else {
+		spec[2] = ERL_DRV_BUF2BINARY, (char*)payload, strlen((char*)payload);
+	}
+	spec[4] = ERL_DRV_TUPLE, 3;
 
-    post_handle_request(response);
-    releaseResponse(response);
+  term = malloc(sizeof(spec));
+	
+	if (NULL != term) {
+		memcpy(term, &spec, sizeof(spec));
+		response_len = sizeof(spec) / sizeof(spec[0]);
+		driver_send_term(port, calleePid, term, response_len);
+	} else {
+		state = OutOfMemory;
+	}
+
+	// now the engine needs the opportunity to free up the result
+  provider->afterTransform(result);
+    
+	// cleanup time...
+	cleanup_job(job);
+	
+	if (state == OutOfMemory) {
+		driver_failure_atom(port, "system_limit");
+	}
+	// TODO: and the other failure conditions?
 };
+
+/* DRIVER API EXPORTS */
 
 static ErlDrvEntry driver_entry = {
-    NULL,                       /* init */
-    start_driver,               /* start, called when port is opened */
-    stop_driver,                /* stop, called when port is closed */
-    NULL,                       /* output, called when port receives messages */
-    NULL,                       /* ready_input, called when input descriptor ready to read*/
-    NULL,                       /* ready_output, called when output descriptor ready to write */
-    "erlxsl_drv",               /* the name of the driver */
-    NULL,                       /* finish, called when unloaded */ //TODO: should we do resource disposal here!?
-    NULL,                       /* handle,  */
-    NULL,                       /* control */
-    NULL,                       /* timeout */
-    outputv,                    /* outputv */
-    ready_async,                /* ready_async, called (from the emulator thread) after an asynchronous call has completed. */
-    NULL,                       /* flush */
-    NULL,                       /* call */
-    NULL                        /* event */
-};
-
-static void init_provider(driver_spec* drv, char* buff) {
-	xsl_engine* engine = (xsl_engine*)driver_alloc(sizeof(xsl_engine));
-	if (engine == NULL) {
-		return;
-	}
-	engine->initialize = default_initialize;
-	engine->handleTransform = default_handleTransform;
-	engine->postHandle = default_postHandle;
-	engine->shutdown = default_shutdown;
-	
-	// NB: this is a temporary structure for testing/discovery
-	d->provider = engine;
-};
-
-static DriverState default_initialize(void* state) {
-	return DriverState.Ok;
-};
-
-static void default_handleTransform(transform_result* result) {
-	result->responseFormat = ResponseFormat.Buffer;
-	result->payload = "<response><name>it works!</name><value>WOW</value></response>";
-	SET_STATUS(result, DriverState.Ok);
-};
-
-static DriverState default_postHandle(transform_result* result) {
-	return DriverState.Ok;
-};
-
-static void default_shutdown(void* state) {
-	ErlDrvPort p = (ErlDrvPort)state;  // just a sanity check! 
+	NULL,                       /* init */
+	start_driver,               /* start, called when port is opened */
+	stop_driver,                /* stop, called when port is closed */
+	NULL,                       /* output, called when port receives messages */
+	NULL,                       /* ready_input, called when input descriptor ready to read*/
+	NULL,                       /* ready_output, called when output descriptor ready to write */
+	"erlxsl_drv",               /* the name of the driver */
+	NULL,                       /* finish, called when unloaded */ //TODO: should we do resource disposal here!?
+	NULL,                       /* handle,  */
+	NULL,                       /* control */
+	NULL,                       /* timeout */
+	outputv,                    /* outputv */
+	ready_async,                /* ready_async, called (from the emulator thread) after an asynchronous call has completed. */
+	NULL,                       /* flush */
+	NULL,                       /* call */
+	NULL                        /* event */
 };
 
 DRIVER_INIT(erlxsl_drv) {
-    return &driver_entry;
+  return &driver_entry;
 }
